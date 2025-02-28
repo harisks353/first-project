fcd9021b2880f657f9e4527956d52e8870c15812
mohammed haris <mohammedharisks600@gmail.com>
2025-02-28 19:19:42 +0100

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ class project {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("mt name is Haris");
-
+        System.out.println("this is my first java project");
     }
 }
 
-    }}
+
+
